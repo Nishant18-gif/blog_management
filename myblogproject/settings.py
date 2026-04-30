3a8bd5b71cc9859d191bc4eea098dbd10281ae95
Nishant18-gif <nishanrpareek89@gmail.com>
2026-04-30 13:16:13 +0530

    switch to sqlite database

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -96,12 +96,8 @@ WSGI_APPLICATION = 'myblogproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',
-        'USER': 'postgres',
-        'PASSWORD': 'jaipur2004',  
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
